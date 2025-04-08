16cd53e8ea2826de5ed62d7a8e619b464c633f5e
OnurKerimoglu <kerimoglu.o@gmail.com>
2025-04-08 19:50:41 +0200

    models.stocks.etf_ticker_weights: join etf_weights with stocks_raw.stock_info

--- models/stocks/etf_ticker_weights.sql
+++ models/stocks/etf_ticker_weights.sql
@@ -1,15 +1,28 @@
 {{ config(
     materialized='table', 
-    alias='etf_' ~ var('etf_symbol') ~ '_weights'
+    alias='etf_' ~ var('etf_symbol') ~ '_tickers'
     ) 
 }}
 
-WITH etf_holdings AS(
+WITH holding_weights AS(
   select
-  ticker,
+  ticker as symbol,
   weight
   from stocks_raw.etfs
   where fund_ticker = '{{ var('etf_symbol') }}'
+),
+etf_tickers_info AS (
+ select
+ hw.symbol,
+ hw.weight,
+ company_name,
+ si.sector,
+ industry,
+ market_capitalization,
+ overall_risk,
+ SAFE_CAST(SPLIT(average_analyst_rating, '-')[OFFSET(0)] AS FLOAT64) AS average_analyst_score
+ from stocks_raw.stock_info si
+ join holding_weights hw ON hw.symbol = si.symbol
 )
-select ticker, weight
-from etf_holdings
+select *
+from etf_tickers_info
